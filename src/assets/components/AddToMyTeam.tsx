a640a826992b9080d121ad81a3ab423d9e2f61cc
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2023-03-12 20:12:52 +0100

    Refacto AddToMyTeam.tsx

--- src/assets/components/AddToMyTeam.tsx
+++ src/assets/components/AddToMyTeam.tsx
@@ -1,5 +1,5 @@
+// Import Packages
 import { useState } from "react";
-
 export default function AddToMyTeam({
   onClick,
   name,
@@ -7,12 +7,20 @@ export default function AddToMyTeam({
   onClick: () => void;
   name: string;
 }) {
-  const [myTeam, setTeam] = useState<boolean>(
-    // Searching inside the Array of Objects if the name of the pokemon is in the local storage
-    JSON.parse(localStorage.getItem("team") || "[]").some(
-      (pokemon: { name: string }) => pokemon.name === name
-    )
+  // Check if the pokemon is in the team
+  const isInTeam = JSON.parse(localStorage.getItem("team") || "[]").some(
+    (pokemon: { name: string }) => pokemon.name === name
   );
+
+  // Check if the team is full
+  const isTeamFull =
+    JSON.parse(localStorage.getItem("team") || "[]").length === 6;
+
+  // Change the text of the button
+  const action = isInTeam ? "Remove from my team" : "Add to my team";
+
+  // Searching inside the Array of Objects if the name of the pokemon is in the local storage
+  const [myTeam, setTeam] = useState<boolean>(isInTeam);
 
   return (
     <>
@@ -23,10 +31,7 @@ export default function AddToMyTeam({
         }}
         className="mx-auto bg-black rounded-lg text-white py-2 px-4 hover:bg-gray-700"
         disabled={
-          JSON.parse(localStorage.getItem("team") || "[]").length === 6 &&
-          !JSON.parse(localStorage.getItem("team") || "[]").some(
-            (pokemon: { name: string }) => pokemon.name === name
-          )
+          isTeamFull && !isInTeam
           // Disable the button if the team is full and the pokemon is not in the team
         }
       >
@@ -38,14 +43,7 @@ export default function AddToMyTeam({
             If the length of the local storage is 6,
             the button will show "Team is full"
         */}
-
-        {JSON.parse(localStorage.getItem("team") || "[]").some(
-          (pokemon: { name: string }) => pokemon.name === name
-        )
-          ? "Remove from my team"
-          : JSON.parse(localStorage.getItem("team") || "[]").length === 6
-          ? "Team is full"
-          : "Add to my team"}
+        {isTeamFull && !isInTeam ? "Team is full" : action}
       </button>
     </>
   );
