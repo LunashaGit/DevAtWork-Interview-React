393ebbba78d6b854f565262e9a4dc19f825320db
Luna Muylkens <93606228+LunashaGit@users.noreply.github.com>
2023-03-12 04:16:37 +0100

    Add Comments in NotFound

--- src/assets/pages/NotFound.tsx
+++ src/assets/pages/NotFound.tsx
@@ -1,8 +1,11 @@
 export default function NotFound() {
   return (
     <div className="flex flex-col text-center h-[100vh] justify-center gap-4 structure">
+      {/* Oops! */}
       <h1 className="text-3xl">Oops!</h1>
+      {/* 404 - Page not found */}
       <h2 className="text-gray-300 italic">404 - Page not found</h2>
+      {/* Link to the homepage */}
       <a href="/" className="text-blue-300 hover:text-blue-400">
         {" "}
         Go back to the homepage
